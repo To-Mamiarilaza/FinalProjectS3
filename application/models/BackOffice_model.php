2d97071e7900be92b630c2ce2eac547f39e40789
JoharyHugo <joharyny@gmail.com>
2023-02-07 15:03:47 +0100

    Changement

---
+++ application/models/BackOffice_model.php
@@ -0,0 +1,12 @@
+<?php
+    if (! defined('BASEPATH')) exit('No direct script access allowed');
+    class BackOffice_model extends CI_Model 
+    {
+        public function insertNewCategorie ($new)
+        {
+           
+        }        
+    }
+    
+
+?>
